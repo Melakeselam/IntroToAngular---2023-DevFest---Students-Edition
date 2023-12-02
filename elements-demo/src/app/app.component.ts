08b81f2883ac185ab0e40dd47df767b294a39309
Melakeselam Moges <melakeselammoges@gmail.com>
2023-12-02 12:17:23 +0300

    finish layout

--- elements-demo/src/app/app.component.ts
+++ elements-demo/src/app/app.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'elements-demo';
   members: string[] = [];
+
+  onAdd(newMember: string) {
+    if (newMember)
+      this.members.push(newMember);
+  }
 }
